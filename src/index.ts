5119f01d51d6c997b60a4dc910f5bd099a593dc3
kazura233 <kazira233@gmail.com>
2021-08-28 21:45:19 +0800

    :tada:

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
-class WXStorage {
-  
+import Taro from '@tarojs/taro'
+
+class WXStorage implements Storage {
+  public get length(): number {
+    const { keys } = Taro.getStorageInfoSync()
+    return keys.length
+  }
+
+  public set lenght(_: number) {
+    throw new Error('readonly')
+  }
+
+  public clear(): void {
+    Taro.clearStorageSync()
+  }
+
+  public getItem(key: string): string | null {
+    return Taro.getStorageSync(key)
+  }
+
+  public key(index: number): string | null {
+    const { keys } = Taro.getStorageInfoSync()
+    if (index >= keys.length) return null
+    return this.getItem(keys[index])
+  }
+
+  public removeItem(key: string): void {
+    Taro.removeStorageSync(key)
+  }
+
+  public setItem(key: string, value: string): void {
+    Taro.setStorageSync(key, value)
+  }
+
+  public all() {
+    const list: {
+      [key: string]: any
+    } = {}
+    const { keys } = Taro.getStorageInfoSync()
+    for (const key of keys) {
+      list[key] = this.getItem(key)
+    }
+    return list
+  }
 }
 
 export default WXStorage
